23707befbdb5ce8631d4dc89265dc2c6137bbbb4
Renan Costa Alencar <renan.costaalencar@gmail.com>
2026-04-23 19:46:33 -0300

    refactor: migrate root navigation to a tab-based layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="update" options={{ title: "Atualização" }} />
-      <Tabs.Screen name="profile" options={{ title: "Perfil" }} />
-    </Tabs>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
